80f9bda93be0656cbeef74e848f199b6f2345091
allsyariff <naelulmuna05@gmail.com>
2025-07-03 23:03:01 +0700

    add dark mode

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -48,12 +48,22 @@ new class extends Component {
                         {{ __('Riwayat Transaksi') }}
                     </x-nav-link>
                 </div>
-
-
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden md:flex md:items-center md:ms-6">
+                <div x-data="{ dark: localStorage.getItem('theme') === 'dark' }" x-init="document.documentElement.classList.toggle('dark', dark)" class="flex items-center">
+                    <label class="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" x-model="dark"
+                            @change="localStorage.setItem('theme', dark ? 'dark' : 'light');
+                        document.documentElement.classList.toggle('dark', dark)"
+                            class="sr-only peer">
+                        <div
+                            class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                        </div>
+                        <span class="ml-2 text-xs font-medium text-gray-900 dark:text-gray-300">🌛</span>
+                    </label>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
