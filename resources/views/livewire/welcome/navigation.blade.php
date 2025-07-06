0b64fe80914a348f59d7f9230cdccc2acd4704a9
allsyariff <naelulmuna05@gmail.com>
2025-07-06 22:48:37 +0700

    feat: update profile-user

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -39,7 +39,7 @@ new class extends Component {
                 @if (Auth::user()->hasRole('Admin'))
                     <a href="/dashboard"
                         class="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">Dashboard</a>
-                    <a href="/profile"
+                    <a href="/profile-user"
                         class="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">Profile</a>
                     <button wire:click="logout"
                         class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 dark:text-red-400 dark:hover:bg-red-900/20 transition duration-150 ease-in-out">
@@ -47,7 +47,7 @@ new class extends Component {
                         {{ __('Log Out') }}
                     </button>
                 @else
-                    <a href="/profile"
+                    <a href="/profile-user"
                         class="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600">Profile</a>
                     <button wire:click="logout"
                         class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50 dark:text-red-400 dark:hover:bg-red-900/20 transition duration-150 ease-in-out">
