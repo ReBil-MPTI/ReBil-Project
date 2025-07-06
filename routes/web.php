788e77c7834143c64e73781ad9c8654febbf9a29
allsyariff <naelulmuna05@gmail.com>
2025-07-06 12:42:21 +0700

    handle if user not admin cannot login in dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Livewire\CarCrud\Cars;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', 'role:Admin'])->group(function () {
     Route::prefix('dashboard')->group(function () {
         Route::view('/', 'dashboard')->name('dashboard');
         Route::view('/profile', 'profile')->name('profile');
         Route::get('/mobil', Cars::class)->name('cars.index');
     });
 });
+
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect('/login');
+});
+
 
 require __DIR__ . '/auth.php';
