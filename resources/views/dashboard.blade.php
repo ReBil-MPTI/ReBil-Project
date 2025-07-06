ede3ce1f47a9e5ee66d69559e12513fd95dd1dc4
allsyariff <naelulmuna05@gmail.com>
2025-07-06 19:27:31 +0700

    add dashboard image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,28 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="py-10 border-2 border-black rounded-xl">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg  ">
+                <div class=" text-gray-900 dark:text-gray-100">
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
+                        <div class="space-y-4">
+                            <h1 class="text-3xl md:text-4xl font-bold text-black dark:text-white">
+                                Selamat Datang <br> {{ Auth::user()->name }}!
+                            </h1>
+                            <p class="text-gray-500 dark:text-gray-300 text-sm md:text-xl text-justify leading-relaxed">
+                                "Tetaplah gigih dan kreatif dalam menjaga harmoni dan kemajuan.
+                                Setiap langkahmu membentuk kenyamanan dan kesuksesan bagi banyak orang."
+                            </p>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/dashboard.png') }}" alt="dashboard-icon"
+                                class="w-full max-w-sm h-auto">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </x-app-layout>
