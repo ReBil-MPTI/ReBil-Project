f05db80e7627cdd86c2dc3f4e7b34263d70b14f5
alinsyarif <alinsyarif04@gmail.com>
2025-07-06 23:29:00 +0700

    hotfix: modal delete

--- resources/views/livewire/user/user-c-r-u-d.blade.php
+++ resources/views/livewire/user/user-c-r-u-d.blade.php
@@ -202,7 +202,7 @@
                 <p class="text-gray-600 mb-6">Apakah kamu yakin ingin menghapus mobil ini?</p>
 
                 <div class="flex justify-end space-x-3">
-                    <button wire:click="$set('confirm$modaldelete', false)"
+                    <button wire:click="$set('modaldelete', false)"
                         class="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400">
                         Batal
                     </button>
