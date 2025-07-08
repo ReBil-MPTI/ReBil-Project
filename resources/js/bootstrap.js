b3876814794d29e9924a1acafefebce5de49720a
allsyariff <naelulmuna05@gmail.com>
2025-07-08 23:17:19 +0700

    update boostrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+let token = document.cookie.match(/XSRF-TOKEN=([^;]+)/);
+if (token) {
+    window.axios.defaults.headers.common["X-XSRF-TOKEN"] = decodeURIComponent(
+        token[1]
+    );
+}
