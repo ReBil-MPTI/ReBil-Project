5c9e4d5208aacdb27dc4f052bd1f3118b5811ec3
allsyariff <naelulmuna05@gmail.com>
2025-07-09 01:20:41 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,17 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'livewire/upload-file'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://rebil-project-production.up.railway.app'],
-    'allowed_origins_patterns' => [],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => ['*'],
+
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+
+    'exposed_headers' => ['*'],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
 ];
 
