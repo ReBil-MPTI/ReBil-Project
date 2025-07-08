8f13f9a61715fdceba6d78041326c8136c001b17
allsyariff <naelulmuna05@gmail.com>
2025-07-08 23:29:28 +0700

    update boostrerap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,8 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-let token = document.cookie.match(/XSRF-TOKEN=([^;]+)/);
+const token = document.head.querySelector('meta[name="csrf-token"]');
+
 if (token) {
     window.axios.defaults.headers.common["X-XSRF-TOKEN"] = decodeURIComponent(
         token[1]
