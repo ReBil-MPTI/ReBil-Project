4b4979271aeee15e1553198f897b249ee39c2797
allsyariff <naelulmuna05@gmail.com>
2025-07-09 21:23:35 +0700

    update boostrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
         ]);
+        $middleware->trustProxies(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
