a4aada49b770ba214e454221a53437976e683532
allsyariff <naelulmuna05@gmail.com>
2025-07-12 13:56:58 +0700

    create migration for add new field db

---
+++ database/migrations/2025_07_12_064739_add_field_detile_car.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('transmission_type')->nullable()->after('car_image');
+            $table->string('engine_capacity')->nullable()->after('transmission_type');
+            $table->string('fuel_type')->nullable()->after('engine_capacity');
+            $table->string('transmission_concept')->nullable()->after('fuel_type');
+            $table->unsignedBigInteger('price')->nullable()->after('transmission_concept');
+            $table->string('seat_capacity')->nullable()->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
