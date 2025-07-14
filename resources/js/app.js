6540dd573a8b3cd84bf2bf2b4bd661d75ef6533c
allsyariff <naelulmuna05@gmail.com>
2025-07-14 20:03:11 +0700

    install aos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+    duration: 800,
+    once: true,
+    easing: "ease-in-out",
+});
+
+document.addEventListener("livewire:load", () => {
+    Livewire.hook("message.processed", () => {
+        AOS.refresh();
+    });
+});
+import "./bootstrap";
